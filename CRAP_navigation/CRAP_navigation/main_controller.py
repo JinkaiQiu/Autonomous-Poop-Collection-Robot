00c38997f7282cc0fc0b45e57fcbef446f0eb3fc
JinkaiQiu <oliverjkqiu@gmail.com>
2023-05-16 19:01:02 -0700

    created custom robot_navigator

--- CRAP_navigation/CRAP_navigation/main_controller.py
+++ CRAP_navigation/CRAP_navigation/main_controller.py
@@ -1,8 +1,26 @@
-import __init__
+import math
+import random
+import time
+import numpy as np
+import rclpy
+from geometry_msgs.msg import (Point, PoseStamped, Quaternion,PointStamped,
+                               Twist)
+from nav_msgs.msg import OccupancyGrid
+# from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
+from robot_navigator import BasicNavigator, TaskResult
+from rclpy.duration import Duration
+from rclpy.node import Node
+from tf2_ros import Buffer, TransformListener
+from nav2_msgs.action import ComputePathToPose
+from action_msgs.msg import GoalStatus
+from nav2_msgs.srv import IsPathValid
+from math import sin, cos, pi
+from rclpy.action import ActionClient
+from serial_motor_demo_msgs.action import Collect
 
 class crap_main_controller (Node):
 	def __init__(self):
-	super().__init__('crap_main_controller')
+		super().__init__('crap_main_controller')
   	
 		# Create Timer
 		timer_period = 1.5
